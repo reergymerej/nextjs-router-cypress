7bece4d05fe6bf4de77ff6e5558a7be38bd4c98a
Jeremy Greer <jex.grizzle@gmail.com>
2025-12-08 08:10:20 -0500

    Add router seam

--- src/helper.ts
+++ src/helper.ts
@@ -1,3 +1,20 @@
-import { useRouter } from "next/navigation";
+"use client";
 
-export { useRouter }
+import {
+  useRouter as useNextRouter,
+} from "next/navigation";
+
+export type UseRouter = typeof useNextRouter;
+let _useRouter: UseRouter | undefined;
+
+export const useRouterSeam = (useRouter: UseRouter) => {
+  _useRouter = useRouter;
+};
+
+export const useRouter = () => {
+  if (!_useRouter) {
+    // assign lazily to support last minute swapping in tests
+    _useRouter = useNextRouter;
+  }
+  return _useRouter();
+};
