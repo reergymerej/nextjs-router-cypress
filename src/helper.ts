d0a21dd8119b48f1a1b5a94efd82001af982a927
Jeremy Greer <jex.grizzle@gmail.com>
2025-12-09 09:01:27 -0500

    Add test seam

--- src/helper.ts
+++ src/helper.ts
@@ -4,6 +4,17 @@ import {
   useRouter as useNextRouter,
 } from "next/navigation";
 
+export type UseRouter = typeof useNextRouter;
+let _useRouter: UseRouter | undefined;
+
+export const useRouterSeam = (useRouter: UseRouter) => {
+  _useRouter = useRouter;
+};
+
 export const useRouter = () => {
-  return useNextRouter()
+  if (!_useRouter) {
+    // assign lazily to support last minute swapping in tests
+    _useRouter = useNextRouter;
+  }
+  return _useRouter();
 };
