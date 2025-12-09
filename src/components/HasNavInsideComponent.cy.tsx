ec0a9c61fc985fbbce3bccda6df60c7f0c194c0c
Jeremy Greer <jex.grizzle@gmail.com>
2025-12-09 09:02:10 -0500

    Inject test router

--- src/components/HasNavInsideComponent.cy.tsx
+++ src/components/HasNavInsideComponent.cy.tsx
@@ -1,4 +1,14 @@
+import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
 import Component from './HasNavInside'
+import { useRouterSeam as injectRouter, UseRouter } from "@/helper";
+
+export const useRouter: UseRouter = () => {
+  return {
+    // mocked router methods
+  } as AppRouterInstance;
+};
+
+injectRouter(useRouter);
 
 describe('<Component />', () => {
   it('renders', () => {
